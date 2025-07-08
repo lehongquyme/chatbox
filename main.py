d83f182b1b0076fef82108ef918256216c6686ae
lehongquyme <lehongquy.me@gmail.com>
2025-07-08 13:13:40 +0700

    Add render.yaml

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi import FastAPI, Form
+from fastapi.responses import FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
 
-
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
+@app.get("/")
+async def serve_index():
+    return FileResponse("static/index.html")
 
 @app.post("/chat")
 async def chat(prompt: str = Form(...)):
-    # Tạm phản hồi giả lập
-    if "tên" in prompt.lower():
-        reply = "Tôi tên là Qfriend, chatbot của bạn!"
-    else:
-        reply = "Xin lỗi, tôi chưa hiểu. Hãy hỏi lại nhé!"
-    return JSONResponse({"response": reply})
+    return JSONResponse({"response": f"Bạn hỏi: {prompt}"})
